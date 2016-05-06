9bf603ee10f2aa810eb9ba012f00bb411b56e5ed
Timothy Steen <tlawrencesteen@gmail.com>
2016-05-06 00:55:14 -0700

    Lives and respawn delay

--- Slingers/Assets/Scripts/Prototype/MortalityScript.cs
+++ Slingers/Assets/Scripts/Prototype/MortalityScript.cs
@@ -5,11 +5,21 @@ public class MortalityScript : MonoBehaviour {
     Vector3 spawn_point;
     public int health_current;
     public int health_max = 3;
+    //a value of -1 indicates infinite lives, 0 will start deactivate on start
+    public int lives = -1;
+    //a value of 0 indiciates instant respawn
+    public float respawn_seconds = 0;
 
 	// Use this for initialization
 	void Start () {
+        if (lives == 0)
+        {
+            gameObject.SetActive(false);
+        }
         health_current = health_max;
         spawn_point = transform.position;
+        if (respawn_seconds < 0)
+            respawn_seconds = 0;
 	}
 	
 	// Update is called once per frame
@@ -29,9 +39,30 @@ public class MortalityScript : MonoBehaviour {
         //check death
         if (health_current <= 0)
         {
-            //respawn
-            transform.position = spawn_point;
-            health_current = health_max;
+            bool infinite_lives = lives == -1;
+            bool has_lives = infinite_lives || lives > 0;
+            //deactivate
+            if (has_lives)
+            {
+                if (respawn_seconds > 0)
+                {
+                    this.gameObject.SetActive(false);
+                    //countdown to respawn
+                    Invoke("Respawn", respawn_seconds);
+                }else
+                {
+                    Respawn();
+                }
+            }
         }
+    }
+
+    public void Respawn()
+    {
+        transform.position = spawn_point;
+        health_current = health_max;
+        this.gameObject.SetActive(true);
+        if(lives > 0)
+            lives--;
     }
 }
