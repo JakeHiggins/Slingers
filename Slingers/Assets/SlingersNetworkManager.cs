4704088075c60d4c06ecc874292864ebbff499ba
Tim Steen <tlawrencesteen@gmail.com>
2016-10-16 00:50:40 -0700

    Add SlingersNetworkManager to initiate client-server multiplayer.

---
+++ Slingers/Assets/SlingersNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+
+public class SlingersNetworkManager : NetworkManager {
+    NetworkConnection gameplayConn;
+    public bool isServer;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isServer)
+        {
+            Debug.AssertFormat(base.StartServer(), "Failed to start a server.");
+        }
+        else
+        {
+            NetworkClient client = base.StartClient();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("Server: Start.");
+    }
+}
