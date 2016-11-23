05c2ea37277f5b0cc418200b53f3c61ae8111eb5
Tim Steen <tlawrencesteen@gmail.com>
2016-11-22 22:44:43 -0800

    Add target killing game (ala smash bros)

---
+++ Slingers/Assets/Scripts/Game Modes/GameModeTargets.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModeTargets : MonoBehaviour {
+    bool game_over = false;
+    bool game_started = false;
+    public TextMesh timer_text;
+    Target[] all_targets;
+    System.DateTime start, finish;
+
+	// Use this for initialization
+	void Start ()
+    {
+        all_targets = GameObject.FindObjectsOfType<Target>();
+        //for(int i = 0; i < )
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        System.TimeSpan timer_time = new System.TimeSpan();
+        Color timer_color = Color.white;
+
+        Target[] all_active_targets = GameObject.FindObjectsOfType<Target>();
+        if (!game_started && all_active_targets.Length != all_targets.Length)
+        {
+            game_started = true;
+            start = System.DateTime.Now;
+        }
+        if(game_started && !game_over)
+        {
+            timer_color = Color.red;
+            bool all_targets_destroyed = all_active_targets.Length == 0;
+            if (all_targets_destroyed)
+            {
+                game_over = true;
+                finish = System.DateTime.Now;
+            }
+            else
+            {
+                timer_time = (System.DateTime.Now - start);
+            }
+        }
+        if(game_over)
+        {
+            timer_color = Color.green;
+            timer_time = finish - start;
+        }
+        timer_text.text = string.Format("{0:d2}.{1:d2}", (int)timer_time.TotalSeconds, timer_time.Milliseconds/10);
+        timer_text.color = timer_color;
+
+        if(Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            
+        }
+	}
+}
