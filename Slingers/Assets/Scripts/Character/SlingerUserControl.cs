e8ee97f2e8a08f47657f5e0caf4b87965f4cac16
Tim Steen <tlawrencesteen@gmail.com>
2016-12-02 19:28:52 -0800

    You can now light attack with the KeyCode:F

--- Slingers/Assets/Scripts/Character/SlingerUserControl.cs
+++ Slingers/Assets/Scripts/Character/SlingerUserControl.cs
@@ -9,18 +9,20 @@ public class SlingerUserControl : Platformer2DUserControl
     public new void FixedUpdate()
     {
         base.FixedUpdate();
-
+        
+        //get input from gamepad and keyboard
         bool gamepad_l_attack = gamepad_enabled && Input.GetKey(gamepad_keycode(2));
         bool gamepad_h_attack = gamepad_enabled && Input.GetKeyDown(gamepad_keycode(3));
         bool gamepad_block = gamepad_enabled && Input.GetKey(gamepad_keycode(4));
         bool gamepad_throw = gamepad_enabled && Input.GetKeyDown(gamepad_keycode(5));
 
-        bool keyboard_l_attack = keyboard_enabled && Input.GetKey(KeyCode.Mouse0);
+        bool keyboard_l_attack = keyboard_enabled && (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.F));
         bool keyboard_h_attack = keyboard_enabled && Input.GetKeyDown(KeyCode.Mouse1);
         bool keyboard_block = keyboard_enabled &&
             (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
         bool keyboard_throw = keyboard_enabled && Input.GetKeyDown(KeyCode.Q);
 
+        //apply input to all equipped weapons
         bool heavy_attack = gamepad_h_attack || keyboard_h_attack;
         bool light_attack = gamepad_l_attack || keyboard_l_attack;
         bool block_shield = gamepad_block || keyboard_block;
@@ -31,6 +33,7 @@ public class SlingerUserControl : Platformer2DUserControl
             weapon.control(light_attack, heavy_attack, block_shield, throw_shield);
         }
         
+        //apply input to weapon pickups
         bool pickup_weapon = gamepad_crouch || keyboard_crouch;
         if (pickup_weapon)
         {
